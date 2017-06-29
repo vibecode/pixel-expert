2cecac64b7329f61c7f6b8531b27d5c4d901cc1d
vibecode <rumashkin@gmail.com>
2017-06-29 17:01:23 +0200

    refactor store configuring

--- src/index.js
+++ src/index.js
@@ -1,27 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/App';
+import { render } from 'react-dom';
+import Root from './components/Root';
 import './styles/index.css';
-import throttle from 'lodash/throttle';
-import { loadState, saveState } from './store/localStorage';
-
-import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
-
 import registerServiceWorker from './registerServiceWorker';
 
-const store = configureStore(loadState());
-
-store.subscribe(throttle(() => {
-  saveState({
-    finalStats: store.getState().finalStats
-  })
-}, 1000));
-
-ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById('root'));
+const store = configureStore();
+console.log(store.getState());
+render(
+    <Root store={store} />,
+    document.getElementById('root')
+);
 
 registerServiceWorker();
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,25 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
+import { loadState, saveState } from './localStorage';
+import throttle from 'lodash/throttle';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default (initialState) => {
-  return createStore(
+export default () => {
+  const persistedState = loadState();
+
+  const store = createStore(
       rootReducer,
-      initialState,
+      persistedState,
       composeEnhancers(applyMiddleware(thunk))
   );
+
+  store.subscribe(throttle(() => {
+    saveState({
+      finalStats: store.getState().finalStats
+    })
+  }, 1000));
+
+  return store;
 }
