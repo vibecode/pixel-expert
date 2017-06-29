4e90f76e6fa6c8e02e1b6b446f3836c07fd2e962
vibecode <rumashkin@gmail.com>
2017-06-29 15:59:58 +0200

    save stats to local storage

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
 import './styles/index.css';
+import throttle from 'lodash/throttle';
+import { loadState, saveState } from './store/localStorage';
 
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 
 import registerServiceWorker from './registerServiceWorker';
 
-const store = configureStore();
+const store = configureStore(loadState());
+
+store.subscribe(throttle(() => {
+  saveState({
+    finalStats: store.getState().finalStats
+  })
+}, 1000));
 
 ReactDOM.render(
     <Provider store={store}>
