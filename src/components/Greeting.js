4695dd2817dee3f977f11f90e0ef32cc04709c02
vibecode <rumashkin@gmail.com>
2017-07-05 23:20:33 +0200

    remake transition with react-transition-group

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -4,43 +4,38 @@ import '../styles/greeting.css';
 import logo from '../img/logo_big.svg';
 import arrow from '../img/arrow_right.svg';
 import { RULES } from '../constants/screenTypes';
+import { CSSTransitionGroup } from 'react-transition-group';
 
 class Greeting extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      mounted: false
-    }
-  }
   handleNextClick = () => {
     this.props.changeScreen(RULES);
   };
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        mounted: true
-      })
-    }, 100)
-  }
-
   render() {
     return (
-        <div className={"greeting " + (this.state.mounted ? 'show' : '')}>
-          <div className="greeting__logo">
-            <img src={logo} width="500" height="200" alt="Pixel Hunter" />
-          </div>
-          <h1 className="greeting__asterisk">*</h1>
-          <div className="greeting__challenge">
-            <h3 className="greeting__title">{strings.GREETING_TITLE}</h3>
-            <div>
-              {strings.GREETING_RULES.split('\n').map((line, idx) => <p key={idx} className="greeting__new-line">{line}</p>)}
+        <CSSTransitionGroup
+            transitionName="greeting"
+            transitionAppear={true}
+            transitionAppearTimeout={500}
+            transitionEnter={false}
+            transitionLeave={false}>
+          <div className="greeting">
+            <div className="greeting__logo">
+              <img src={logo} width="500" height="200" alt="Pixel Hunter" />
+            </div>
+            <h1 className="greeting__asterisk">*</h1>
+            <div className="greeting__challenge">
+              <h3 className="greeting__title">{strings.GREETING_TITLE}</h3>
+              <div>
+                {strings.GREETING_RULES.split('\n').map((line, idx) => <p key={idx}
+                                                                          className="greeting__new-line">{line}</p>)}
+              </div>
+            </div>
+            <div className="greeting__continue" onClick={this.handleNextClick}>
+              <img src={arrow} width="100" height="100" alt="Next" />
             </div>
           </div>
-          <div className="greeting__continue" onClick={this.handleNextClick}>
-              <img src={arrow} width="100" height="100" alt="Next" />
-          </div>
-        </div>
+        </CSSTransitionGroup>
     );
   }
 }
