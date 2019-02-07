08e765b3096e29aff5ab39b125866b54d92dacd7
vibecode <rumashkin@gmail.com>
2019-02-07 12:34:33 +0100

    fix container width

--- src/index.js
+++ src/index.js
@@ -2,19 +2,8 @@ import React from 'react'
 import { render } from 'react-dom'
 import Root from './components/Root'
 import './index.scss'
-import './transitions.css'
 import configureStore from './store/configureStore'
 import registerServiceWorker from './registerServiceWorker'
-
-window.requestAnimFrame =
-  window.requestAnimationFrame ||
-  window.webkitRequestAnimationFrame ||
-  window.mozRequestAnimationFrame ||
-  window.oRequestAnimationFrame ||
-  window.msRequestAnimationFrame ||
-  function(callback) {
-    window.setTimeout(callback, 1000 / 60)
-  }
 
 const store = configureStore()
 
